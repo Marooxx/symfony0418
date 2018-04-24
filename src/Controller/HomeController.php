4ef20b0b1d3e8baff9dd2175ab605fba0a3539c1
jerome <test@test.fr>
2018-04-24 11:28:14 +0200

    Gestion des erreurs (404 et generales)

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller {
+
+    /**
+     * @Route("/index")
+     * @throws \Exception
+     */
+    public function HomePage(): Response {
+
+        // La ligne suivante génère une erreur 500
+        throw new \Exception('Erreur perso');
+
+        $titreH1='Page d\'accueil';
+
+        return $this->render('index.html.twig', [
+            'title' => $titreH1
+        ]);
+        
+    }
+    public function contact(): Response {
+        
+        $titreContact = "contact";
+        return $this->render('contact.html.twig',[
+            'titre'=>$titreContact
+        ]);
+    }
+    
+    /** 
+    *@Route("/redirige-moi")
+    */
+        
+    public function redirige(): Response {
+        
+        return $this->redirectToRoute('app_home_homepage');
+    }
+    
+    /**
+     *  @Route("/blog/{nbpage}", requirements={"nbpage"="\d+"})
+     */
+    public function blog($nbpage): Response {
+        return $this->render('blog.html.twig',[
+            'page'=>$nbpage
+        ]);
+    }
+    
+    /**
+     *  @Route("/blog/accueil")
+     */
+    public function blog2(): Response {
+        return $this->render('blog2.html.twig',[
+            'page'=>0
+        ]);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
